f3d8de29e23d16e18559eed82a4661f5adfd6d65
Radwan Faris <radwanfaris13@gmail.com>
2022-10-24 14:18:24 +0200

    added class:ParsedData

--- free4talk_scraper.py
+++ free4talk_scraper.py
@@ -1,29 +1,44 @@
 import requests
 
 
-class Scraper():
-    url = "https://free4talk-sync.herokuapp.com/sync/get/free4talk/groups/?a=sync-get-free4talk-groups"
+class JsonRequest():
+
+    __url = "https://free4talk-sync.herokuapp.com/sync/get/free4talk/groups/?a=sync-get-free4talk-groups"
     
-    def __init__(self):
-        self.url = self.url
+    status = None
     
-    def get_json_data(self):
-        res =requests.post(self.url)
+    @classmethod
+    def get_json_data(cls):
+        res =requests.post(cls.__url)
         res.encoding='utf-8-sig'
+        cls.status = res.status_code
         return res.json()
     
-    def __str__(self):
-        return str(self.requ.status)       
+    @classmethod
+    def __str__(cls):
+        return cls.status       
 
 
-request_object = Scraper()
-data = request_object.get_json_data()
-x = 0
-for room in data["data"]:
-    for clients in data["data"][room]["clients"] :
-        for client in clients:
-            x += 1
-            print(client)
-        print("##" * 10)
-print(type(data))
-print(x)
+class ParsedData():
+    
+    def __init__(self):
+        self.__data = JsonRequest.get_json_data()
+        self.status = JsonRequest.status
+        self.num_users = 0
+    
+    def extract_data(self):
+        for room in self.__data["data"]:
+            for user in self.__data["data"][room]["clients"] :
+                self.num_users += 1
+                print(f'id: {user["id"]} name: {user["name"]} followers: {user["followers"]}')
+    
+    def __str__(self):     
+        return F"{self.num_users}"
+
+    
+
+ob = ParsedData()
+ob.extract_data()
+print(ob)
+print(ob.status)
+print(JsonRequest.__str__())
