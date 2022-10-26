bdf4ada1767187f66d25399153841a77b4c82dd6
Radwan Faris <radwanfaris13@gmail.com>
2022-10-26 12:39:39 +0200

    added test funcs for each class in scraper

--- test_free4talk_scraper.py
+++ test_free4talk_scraper.py
@@ -1,17 +1,29 @@
-import pytube
-from .free4talk_scraper import JsonRequest, ParsedData, SaveAsCsv
+import pytest
+import requests
+
+from free4talk_scraper import JsonRequest, ParsedData, SaveAsCsv
+
+_data = 0
+
+def test_JsonRequest():
+    global _data
+    _data = JsonRequest.get_json_data()
+    assert _data
+    assert JsonRequest.status == 200
+
+    
+
+def test_ParsedData():
+    parser = ParsedData(_data)
+    assert parser._header == ["Name", "Id", "Followers", "following", "friends"]
+    lis = list(parser.extracting_data())
+    assert len(lis) >= 1  
 
 
-class test_JsonRequest:
-    ...
-    # TODO
+def test_SaveAsCsv():
+    ob = SaveAsCsv("test.csv")
+    assert ob.file == "test.csv"
+    assert ob._status == 200
+    assert ob.save() 
+    assert ob.users >= 1
 
-
-class test_ParsedData:
-    ...
-    # TODO
-
-
-class test_SaveAsCsv:
-    ...
-    # TODO
